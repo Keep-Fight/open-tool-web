f0c833e5a1f497d3bbb102e5a77ce305b10722c1
lhd <lhd86@outlook.com>
2026-04-16 21:37:20 +0800

    feat: @路径配置

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import svgLoader from 'vite-svg-loader'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -21,11 +22,16 @@ export default defineConfig({
             }
         }
     },
-
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
         minify: 'esbuild',
         sourcemap: false,
+    },
+    resolve: {
+        alias: {
+            // 配置 @ 指向 src 目录
+            '@': path.resolve(__dirname, 'src')
+        }
     }
 })
